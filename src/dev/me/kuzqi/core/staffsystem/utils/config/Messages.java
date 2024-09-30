382d63159c48116ba29b3cb244cc704d7a4c66a6
kuzqi <112808292+kuzqi@users.noreply.github.com>
2024-09-30 18:19:02 +0300

    Update Messages.java

--- src/dev/me/kuzqi/core/staffsystem/utils/config/Messages.java
+++ src/dev/me/kuzqi/core/staffsystem/utils/config/Messages.java
@@ -18,32 +18,32 @@ public enum Messages {
     
     BLOCKED_MSG("chat-manager.blocked-msg"),
     ANTI_SPAM("chat-manager.anti-spam"),
-	CLEAR_CHAT("chat-manager.cleared-chat"),
+    CLEAR_CHAT("chat-manager.cleared-chat"),
     LOCKED_CHAT("chat-manager.locked-chat"),
     UNLOCKED_CHAT("chat-manager.unlocked-chat"),
     BORADCAST_FORMAT("chat-manager.broadcast-format"),
 	
-	NON_ITEM("commands-utils.non-item"),
-	NOT_ALLOWED("commands-utils.not-allowed"),
-	NOT_ALLOWED_FORSELF("commands-utils.not-allowed-forself"),
-	ONLY_PLAYER_CMD("commands-utils.only-player-cmd-execute"),
-	UNKNOWN_PLAYER("commands-utils.unknown-player"),
-	UNKNOWN_RANK("commands-utils.unknown-rank"),
+    NON_ITEM("commands-utils.non-item"),
+    NOT_ALLOWED("commands-utils.not-allowed"),
+    NOT_ALLOWED_FORSELF("commands-utils.not-allowed-forself"),
+    ONLY_PLAYER_CMD("commands-utils.only-player-cmd-execute"),
+    UNKNOWN_PLAYER("commands-utils.unknown-player"),
+    UNKNOWN_RANK("commands-utils.unknown-rank"),
 	
-	FLY_CREATIVE_MOD("fly-msg.creative-mode"),
-	FLY_ENABLED("fly-msg.fly-enable"),
-	FLY_DISABLED("fly-msg.fly-disable"),
+    FLY_CREATIVE_MOD("fly-msg.creative-mode"),
+    FLY_ENABLED("fly-msg.fly-enable"),
+    FLY_DISABLED("fly-msg.fly-disable"),
 	
-	VANISH_ENABLE("vanish-msg.vanish-enable"),
-	VANISH_DISABLE("vanish-msg.vanish-disable"),
-	VANISH_HEADER_LIST("vanish-msg.vanish-list"),
-	VANISH_EMPTY_LIST("vanish-msg.empty-vanish-list"),
-	VANISH_TP_RULE("vanish-msg.vanishtp-rule"),
+    VANISH_ENABLE("vanish-msg.vanish-enable"),
+    VANISH_DISABLE("vanish-msg.vanish-disable"),
+    VANISH_HEADER_LIST("vanish-msg.vanish-list"),
+    VANISH_EMPTY_LIST("vanish-msg.empty-vanish-list"),
+    VANISH_TP_RULE("vanish-msg.vanishtp-rule"),
 	
-	PLUGIN_BY("plugin-copyright.plugin-by"),
-	PLUGIN_ENABLE("plugin-copyright.plugin-startup-msg"),
-	PLUGIN_DISABLE("plugin-copyright.plugin-shutdown-msg"),
-	PLUGIN_RELOAD("plugin-copyright.plugin-reload");
+    PLUGIN_BY("plugin-copyright.plugin-by"),
+    PLUGIN_ENABLE("plugin-copyright.plugin-startup-msg"),
+    PLUGIN_DISABLE("plugin-copyright.plugin-shutdown-msg"),
+    PLUGIN_RELOAD("plugin-copyright.plugin-reload");
 	
     private final String msg;
 
